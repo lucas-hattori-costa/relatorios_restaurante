fad431c8b5c4c539cdc7c9f4f765e715370ff644
lucas-hattori-costa <60410592+lucas-hattori-costa@users.noreply.github.com>
2020-05-15 06:02:35 -0300

    Extrato e Produtos - 2018 a 2020

---
+++ pdf_reader.py
@@ -0,0 +1,24 @@
+import os
+from PyPDF2 import PdfFileReader
+
+def get_info(path):
+    with open(path, 'rb') as f:
+        pdf = PdfFileReader(f)
+        info = pdf.getDocumentInfo()
+        number_of_pages = pdf.getNumPages()
+    
+    print(info)
+
+def text_extractor(path):
+    with open(path, 'rb') as f:
+        pdf = PdfFileReader(f)
+        # get the first page
+        page = pdf.getPage(1)
+        print(page)
+        print('Page type: {}'.format(str(type(page))))
+        text = page.extractText()
+        print(text)
+
+if __name__ == '__main__':
+    path = './dezembro 2018.pdf'
+    text_extractor(path)
